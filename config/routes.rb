8b976ab0cd0b64c382919a5673276fd8be8b2c58
西影行 <richardchen0954@gmail.com>
2018-05-20 16:25:38 +0800

    implement my posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :groups do
     resources :posts
   end
+
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
   root "groups#index"
 end
